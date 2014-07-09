187d2d174cc44d593915a9f2d8697043fd605c30
Paul <peiche@gmail.com>
2014-07-09 17:28:44 -0500

    Made directory reference a little smarter.

--- fluidbox-wp.php
+++ fluidbox-wp.php
@@ -15,9 +15,9 @@ the Free Software Foundation; either version 2 of the License, or
 (at your option) any later version.
 */
 
-wp_enqueue_style( 'jquery.fluidbox', WP_PLUGIN_URL . '/fluidbox/css/fluidbox.css', false, '1.3.3' );
+wp_enqueue_style( 'jquery.fluidbox', plugins_url( 'css/fluidbox.css', __FILE__ ), false, '1.3.3' );
 
-wp_enqueue_script( 'jquery.fluidbox', WP_PLUGIN_URL . '/fluidbox/js/jquery.fluidbox.min.js', array( 'jquery' ), '1.3.3', true );
-wp_enqueue_script( 'jquery.fluidboxcustom', WP_PLUGIN_URL . '/fluidbox/js/fluidbox.custom.js', array( 'jquery', 'jquery.fluidbox' ), '1.0', true );
+wp_enqueue_script( 'jquery.fluidbox', plugins_url( 'js/jquery.fluidbox.min.js', __FILE__ ), array( 'jquery' ), '1.3.3', true );
+wp_enqueue_script( 'jquery.fluidboxcustom', plugins_url( 'js/fluidbox.custom.js', __FILE__ ), array( 'jquery', 'jquery.fluidbox' ), '1.0', true );
 
 ?>
